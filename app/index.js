f1a4dca75f9ebccaadf3e914aae460b56ab91194
Fearful <joan.lascano@gmail.com>
2014-07-17 17:55:57 -0300

    Inittial Commit yeoman

---
+++ app/index.js
@@ -0,0 +1,59 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+
+var MeanpModulesGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay('Welcome to the meanp module!'));
+
+    var prompts = [{
+      name: 'moduleName',
+      message: 'How would you like to name your module?',
+      default: true
+    },
+    {
+      type: 'confirm'
+      name: 'testing',
+      message: 'Would you like to generate simple tests?',
+      default: true
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.someOption = props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  app: function () {
+    this.mkdir('app');
+    this.mkdir('app/templates');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = MeanpModulesGenerator;
