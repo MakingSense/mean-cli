6b314633383a88223c06110ef6fb9ba10ff0b7ae
Max <max.z_@hotmail.com>
2016-06-30 16:54:03 -0300

    Adding missing cfg validation

--- login/index.js
+++ login/index.js
@@ -16,33 +16,13 @@ var CartGenerator = meanpGen.extend({
                 action: parts[1]
             };
         })[0];
-        var dirPath = '/templates';
-        this.sourceRoot(_path.join(__dirname, dirPath));
-
-        this.on('end', function () {
-            if (!this.options['skip-install']) {
-                this.installDependencies({
-                    npm: true,
-                    bower: true,
-                    skipInstall: false
-                });
-            }
-        });
     },
 
     createModuleFiles: function () {
         var done = this.async();
         // Have Yeoman greet the user.
-        this.log(yosay('This is the meanp login generator!'));
-        var prompts = [{
-            name: 'moduleName',
-            message: 'How would you like to name your login?',
-            default: this.attrs ? this.attrs.name : 'login'
-        }];
-        this.prompt(prompts, function (props) {
-            this.moduleName = props.moduleName;
-            done();
-        }.bind(this));
+        this.log(yosay('This is the meanp login generator. Let me configure the module'));
+        done();
     },
     afterConfirm: function(){
         var base = this.dest._base;
@@ -56,17 +36,21 @@ var CartGenerator = meanpGen.extend({
         var apiHook = "/*===== login hook =====*/";
         var modulesHook1 = "//===== meanp-cli hook =====//";
         var modulesHook2 = "//===== meanp-cli login hook =====//";
+        var modulesHook3 = "//===== meanp-cli login cgf hook =====//";
         var loginHook = "<!-- //===== meanp-cli login hook =====// -->";
         
         // Values to insert
         var loginInsert = "authenticationMiddleware.verifySignature, authenticationMiddleware.verifySecret, authorizationMiddleware.getAuthorizationFn('menu', 'view'),";
-        var modulesInsert = ".when('/login', { templateUrl: 'modules/base/views/login.html', controller: 'LoginCtrl', requireAuth: false }).when('/signup', { templateUrl: 'modules/base/views/signup.html', controller: 'SignupCtrl', requireAuth: false})";
+        var modulesInsert1 = ".when('/login', { templateUrl: 'modules/base/views/login.html', controller: 'LoginCtrl', requireAuth: false })" + '\n  ' + ".when('/signup', { templateUrl: 'modules/base/views/signup.html', controller: 'SignupCtrl', requireAuth: false})";
+        var modulesInsert2 = "var nextPath = $location.path();var nextRoute = $route.routes[nextPath];" + '\n  '  + "  if (nextRoute && nextRoute.requireAuth && !authService.isAuthed()) {$location.path('/login');}";
+        var modulesInsert3 = "configService.get().then(function() {auth.init({domain: $localStorage.auth0Domain,clientID: $localStorage.auth0ClientId,loginUrl: '/login'});}, function(err) {});";
         var loginInsertHtml = "<li><a href='#'>Welcome, {{ getCurrentUser().username }}</a></li><li><a href=' ng-click='logout()'>Logout</a></li>";
 
         fs.writeFileSync(base + _path.sep + 'api' + _path.sep + 'base' + _path.sep + 'routes' + _path.sep + 'base.js', apiBase.replace(apiHook, loginInsert + '\n  ' + apiHook));
 
-        modulesApp = modulesApp.replace(modulesHook1, modulesInsert + '\n  ' + modulesHook1)
-        fs.writeFileSync(base + _path.sep + 'public' + _path.sep + 'modules' + _path.sep + 'app.js', modulesApp.replace(modulesHook2, modulesInsert + '\n  ' + modulesHook2));
+        modulesApp = modulesApp.replace(modulesHook1, modulesInsert1 + '\n  ' + modulesHook1)
+        modulesApp = modulesApp.replace(modulesHook2, modulesInsert2)
+        fs.writeFileSync(base + _path.sep + 'public' + _path.sep + 'modules' + _path.sep + 'app.js', modulesApp.replace(modulesHook3, modulesInsert3));
         fs.writeFileSync(base + _path.sep + 'public' + _path.sep + 'modules' + _path.sep + 'base' + _path.sep + 'views' + _path.sep + 'partials' + _path.sep + 'navbar.html', navBarHtml.replace(loginHook, loginInsertHtml));
     }
 });
